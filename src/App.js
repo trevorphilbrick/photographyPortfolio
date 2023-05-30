aa0bddad3f4427581c2578f3bb0ed827bb6fd122
Trevor Philbrick <trevorphilbrick@outlook.com>
2023-05-30 15:07:03 -0700

    initializing analytics

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { getFirestore } from "firebase/firestore";
 import ToggleTheme from "./components/common/ToggleTheme";
 import Footer from "./components/common/Footer";
 import FabContainer from "./components/common/FabContainer";
+import { getAnalytics } from "firebase/analytics";
 
 const baseTheme = {
   typography: {
@@ -45,8 +46,11 @@ const lightTheme = createTheme({
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+// Initialize Analytics and get a reference to the service
+const analytics = getAnalytics(app);
 
 const FirebaseDBContext = createContext();
+const FirebaseAnalyticsContext = createContext();
 const AppStateContext = createContext();
 const SetThemeContext = createContext();
 
@@ -55,24 +59,26 @@ function App() {
   const [appState, setAppState] = useState({});
 
   return (
-    <SetThemeContext.Provider value={{ setThemeMode, themeMode }}>
-      <AppStateContext.Provider value={{ appState, setAppState }}>
-        <FirebaseDBContext.Provider value={db}>
-          <ThemeProvider theme={themeMode === "dark" ? darkTheme : lightTheme}>
-            <CssBaseline />
-            <div className="App">
-              <ToggleTheme />
-              <FabContainer />
-              <Outlet />
-              <Footer />
-            </div>
-          </ThemeProvider>
-        </FirebaseDBContext.Provider>
-      </AppStateContext.Provider>
-    </SetThemeContext.Provider>
+    <FirebaseAnalyticsContext.Provider value={analytics}>
+      <SetThemeContext.Provider value={{ setThemeMode, themeMode }}>
+        <AppStateContext.Provider value={{ appState, setAppState }}>
+          <FirebaseDBContext.Provider value={db}>
+            <ThemeProvider theme={themeMode === "dark" ? darkTheme : lightTheme}>
+              <CssBaseline />
+              <div className="App">
+                <ToggleTheme />
+                <FabContainer />
+                <Outlet />
+                <Footer />
+              </div>
+            </ThemeProvider>
+          </FirebaseDBContext.Provider>
+        </AppStateContext.Provider>
+      </SetThemeContext.Provider>
+    </FirebaseAnalyticsContext.Provider>
   );
 }
 
-export { FirebaseDBContext, AppStateContext, SetThemeContext };
+export { FirebaseDBContext, AppStateContext, SetThemeContext, FirebaseAnalyticsContext };
 
 export default App;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,22 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import Hero from "../components/home/Hero";
 import Galleries from "../components/home/Galleries";
 import About from "../components/home/About";
+import { logEvent } from "firebase/analytics";
+import { FirebaseAnalyticsContext } from "../App";
 
 const Home = () => {
   const [isLoaded, setIsLoaded] = useState(false);
+
+  const analytics = useContext(FirebaseAnalyticsContext);
+
+  useEffect(() => {
+    logEvent(analytics, "page_view", {
+      page_title: "Home",
+      page_location: "/",
+      page_path: "/"
+    });
+  }, []);
 
   useEffect(() => {
     console.log(isLoaded);

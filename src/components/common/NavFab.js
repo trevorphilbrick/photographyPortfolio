5f715397d2708995947af349091d9cb83bbee71f
Trevor Philbrick <trevorphilbrick@outlook.com>
2023-05-30 14:05:18 -0700

    fixed navbar text color

--- src/components/common/NavFab.js
+++ src/components/common/NavFab.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { Fab } from "@mui/material";
+import { Fab, useTheme } from "@mui/material";
 import { HashLink } from "react-router-hash-link";
 
 const NavFab = () => {
+  const theme = useTheme();
   return (
     <Fab size="medium" variant="extended">
-      <HashLink to="/#galleries">Galleries</HashLink>
+      <HashLink to="/#galleries" smooth style={{ textDecoration: "none", color: theme.palette.text.primary }}>
+        Galleries
+      </HashLink>
     </Fab>
   );
 };
